a67bfe44d34ab78b946bee87516ebf7343112661
Gurvindar singh <132663008+Gscoder27@users.noreply.github.com>
2025-09-08 11:01:34 +0530

    Initial commit with C# programming files

--- obj/Debug/net10.0/C#-Programming.AssemblyInfo.cs
+++ obj/Debug/net10.0/C#-Programming.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#-Programming")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94de003ffbf0d15042e6fb094db9eab3d3a82229")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#-Programming")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#-Programming")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
